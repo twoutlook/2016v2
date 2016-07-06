1f2d9e98df0a0e7b70283025259b67ca56c9c974
Mark <twcloudwebapp@outlook.com>
2016-07-06 16:02:35 +0800

    show my Chinese name

--- fingerprint/finger-login-checker-v2.php
+++ fingerprint/finger-login-checker-v2.php
@@ -29,10 +29,10 @@ if ($device_id != "abc987") {
  */
 
 
-$nameList[611] = "test111";
+$nameList[611] = "mark.chen";
 $nameList[612] = "test222";
 $nameList[613] = "test333";
-$nameZhList[611] = "左手食指";
+$nameZhList[611] = "陳炳陵(左手食指)";
 $nameZhList[612] = "左手中指";
 $nameZhList[613] = "左手無名指";
 
